69e818c3002ce954433bb96576cd269a36ef90e1
DanLucas200123 <danlucas200123@gmail.com>
2021-02-04 20:52:06 +0000

    Added a fade-out for player death Added a pause screen

---
+++ GGJ21/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    void Start()
+    {
+        setPaused(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            setPaused(true);
+        }
+    }
+
+    void setPaused(bool paused)
+    {
+        foreach (Transform child in gameObject.transform)
+        {
+            child.gameObject.SetActive(paused);
+        }
+
+        if (paused)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void unpause()
+    {
+        setPaused(false);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
