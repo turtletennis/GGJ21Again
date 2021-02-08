c5141b5d97b9edecb8c1bd0e848a3417cd1f6c45
SHHH <66333568+SamHSoundDesign@users.noreply.github.com>
2021-02-08 12:15:16 +0000

    Toyroom lighting & falling blocks

---
+++ GGJ21/Assets/ReloadFallingObj.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReloadFallingObj : MonoBehaviour
+{
+    [SerializeField] Vector3 respawnPosition;
+    [SerializeField] Quaternion respawnRotation;
+ 
+    public GameObject blockPrefab;
+
+    //public GameObject playerPefab;
+
+    [SerializeField] Vector3 respawnPlayerPosition;
+    [SerializeField] Quaternion respawnPlayerRotation;
+
+    [SerializeField] GameObject playereRespawnPosition;
+    //[SerializeField] Quaternion respawnRotation;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per fram
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (blockPrefab == null) { return; }
+        if (other.CompareTag("Blocks"))
+        {
+
+            //SpawnBlock();
+
+            //newBlock.transform.position = respawnPosition;
+            Instantiate(blockPrefab, respawnPosition , respawnRotation);
+            //respawnPosition.z = respawnPosition.z + 10;
+
+            Destroy(other.gameObject);
+        }
+
+        if (other.CompareTag("Player"))
+        {
+            other.transform.position = playereRespawnPosition.transform.position;
+        }
+
+
+    }
+
+
+}   
---
+++ GGJ21/Assets/Respawn.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    [SerializeField] GameObject playerPefab;
+    [SerializeField] GameObject respawnPosition;
+    private Vector3 respawnPositionV3;
+    [SerializeField] Quaternion respawnRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    //    respawnPositionV3 = respawnPosition.transform.position;
+    //    respawnRotation = gameObject.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    
+     //if (other.gameObject.CompareTag("Respawn"))
+     //   {
+
+     //       //SpawnBlock();
+     //       Debug.Log("HHHH");
+     //       //newBlock.transform.position = respawnPosition;
+     //       ///Instantiate(playerPefab, respawnPosition, respawnRotation);
+     //       //respawnPosition.z = respawnPosition.z + 10;
+     //       gameObject.transform.position = respawnPositionV3;
+     //       //Destroy(gameObject);
+     //   }
+}
+}
