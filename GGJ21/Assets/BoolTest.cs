2206fd1c2df73fa90a25843236ab13959db82b6a
SamH <66333568+SamHSoundDesign@users.noreply.github.com>
2021-02-08 17:46:15 +0000

    Revert "Merge branch 'master' of https://github.com/turtletennis/GGJ21Again"
    This reverts commit d34584acbe961b3a5978993c2b6126be1f974a33, reversing
    changes made to 1d807bd965ab097b490fb1ee0de222dd16487afa.

---
+++ GGJ21/Assets/BoolTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoolTest : MonoBehaviour
+{
+   public bool isBeach;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // Debug.Log(isBeach);
+    }
+}
---
+++ GGJ21/Assets/LocationSet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationSet : MonoBehaviour
+{
+    public string location = "Beach";
+    [SerializeField] GameObject playerSounds = null;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if(playerSounds ==null) { return; }
+        
+        if(location == "Beach")
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
