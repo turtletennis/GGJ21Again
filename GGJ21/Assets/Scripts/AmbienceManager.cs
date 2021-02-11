b75a5c83d5a677654a582a3cd9c36c8138047ffd
SHHH <66333568+SamHSoundDesign@users.noreply.github.com>
2021-02-11 22:31:20 +0000

    Update ambience manager script

---
+++ GGJ21/Assets/Scripts/AmbienceManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AmbienceManager : MonoBehaviour
+{
+    private AudioSource ambienceSoundEmitter;
+    [SerializeField] AudioClip ambienceSFXLoop;
+    [SerializeField] string Level = "PlayRoom";
+    // Start is called before the first frame update
+    void Start()
+    {
+        ambienceSoundEmitter = GetComponent<AudioSource>();
+        if (Level == "Beach")
+        {
+            ambienceSoundEmitter.loop = true;
+            ambienceSoundEmitter.clip = ambienceSFXLoop;
+            ambienceSoundEmitter.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
