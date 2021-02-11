f4437904152ec343dfd5ce9df2a8239e3e0cd9d0
SamH <66333568+SamHSoundDesign@users.noreply.github.com>
2021-02-11 13:52:34 +0000

    AmbientSound start

---
+++ GGJ21/Assets/Ambient3DSound.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class Ambient3DSound : MonoBehaviour
+{
+    private AudioSource soundEmitter = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (soundEmitter == null)
+        {
+            return;
+        }
+
+        soundEmitter = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("TriggerEnter");
+    }
+
+}
