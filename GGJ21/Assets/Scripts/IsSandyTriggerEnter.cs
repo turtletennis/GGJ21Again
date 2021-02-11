84075783bd79fb849de44cd0a7677e93432c2917
SHHH <66333568+SamHSoundDesign@users.noreply.github.com>
2021-02-11 22:28:05 +0000

    Update isSandy rtigger script

---
+++ GGJ21/Assets/Scripts/IsSandyTriggerEnter.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsSandyTriggerEnter : MonoBehaviour
+{
+    public GameObject character;
+    private playerSounds characterPlayerSounds;
+    public bool makeTransitionOnEnterSandy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterPlayerSounds = character.GetComponent<playerSounds>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (makeTransitionOnEnterSandy == true)
+        {
+            if (other.CompareTag("Respawn"))
+            {
+                characterPlayerSounds.ifSand = true;
+            }
+
+        }
+        else
+        {
+            if (other.CompareTag("Respawn"))
+            {
+                characterPlayerSounds.ifSand = false;
+            }
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (makeTransitionOnEnterSandy == true)
+        {
+            if (other.CompareTag("Respawn"))
+            {
+                characterPlayerSounds.ifSand = false;
+            }
+
+        }
+        else
+        {
+            if (other.CompareTag("Respawn"))
+            {
+                characterPlayerSounds.ifSand = true;
+            }
+        }
+    }
+}
