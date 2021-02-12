4af0c1c90caa996a3327c7a992d282ba44de8140
SamH <66333568+SamHSoundDesign@users.noreply.github.com>
2021-02-12 16:17:44 +0000

    Audio tweaks

---
+++ GGJ21/Assets/Scripts/MusicManager2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+class MusicManager2 : MonoBehaviour
+{
+    private AudioSource musicEmitter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicEmitter = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void StopMusic()
+    {
+        musicEmitter.Stop();
+    }
+
+    public void StartMusicOnLevelLoad(AudioClip _audioClip)
+    {
+        musicEmitter.loop = true;
+        musicEmitter.clip = _audioClip;
+        musicEmitter.Play();
+    }
+}
